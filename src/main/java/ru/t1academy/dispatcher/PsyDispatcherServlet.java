65e1c9ed19a37e13fe31b9a05d174022a31b789c
Ivan Nizalzov <ifn@tvip.ru>
2024-02-13 13:09:32 +0300

    refactored DispatcherServlet

--- src/main/java/ru/t1academy/dispatcher/PsyDispatcherServlet.java
+++ src/main/java/ru/t1academy/dispatcher/PsyDispatcherServlet.java
@@ -40,70 +40,11 @@ public class PsyDispatcherServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
         response = getRequestHandler.handleRequest(controllerMap, request, response, objectMapper);
-
-        /*controllerMap.forEach((type, object) -> {
-            Optional<Method> first = Arrays.stream(type.getMethods())
-                    .filter(method -> method.isAnnotationPresent(GetMapping.class) &&
-                            method.getAnnotation(GetMapping.class).value().equals(request.getRequestURI())
-                    )
-                    .findFirst();
-
-            if (first.isPresent()) {
-                try {
-                    Object invoke = first.get().invoke(object);
-                    response.setStatus(200);
-                    response.setContentType("application/json");
-                    response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-                    response.getWriter().print(objectMapper.writeValueAsString(invoke));
-                } catch (IllegalAccessException | InvocationTargetException | IOException e) {
-                    response.setStatus(500);
-                }
-            } else {
-                response.setStatus(404);
-            }
-        });*/
-
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
         response = postRequestHandler.handleRequest(controllerMap, request, response, objectMapper);
-
-        /*controllerMap.forEach((type, object) -> {
-            Optional<Method> first = Arrays.stream(type.getMethods())
-                    .filter(method -> method.isAnnotationPresent(PostMapping.class) &&
-                            method.getAnnotation(PostMapping.class).value().equals(request.getRequestURI()))
-                    .findFirst();
-
-            if (first.isPresent()) {
-                try {
-                    StringBuilder sb = new StringBuilder();
-                    request.getReader().lines().forEach(sb::append);
-                    Optional<Class<?>> parameter = Arrays.stream(first.get().getParameterTypes()).findFirst();
-                    Object result;
-
-                    if (parameter.isPresent()) {
-                        result = first.get().invoke(object, objectMapper.readValue(sb.toString(), parameter.get()));
-                    } else {
-                        result = first.get().invoke(object);
-                    }
-
-                    response.setContentType("application/json");
-                    response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-                    if (result != null) {
-                        response.setStatus(200);
-                        response.getWriter().print(objectMapper.writeValueAsString(result));
-                    } else {
-                        response.setStatus(204);
-                    }
-                } catch (IllegalAccessException | InvocationTargetException | IOException e) {
-                    response.setStatus(500);
-                }
-            } else {
-                response.setStatus(404);
-            }
-        });*/
-
     }
 
 }
