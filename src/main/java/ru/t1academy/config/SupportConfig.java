00f61fac2134f36e669a6222e35d1fcca33c6f9d
Ivan Nizalzov <ifn@tvip.ru>
2024-02-19 20:45:29 +0300

    feat: added Spring Boot 3.x, Lombok, In-Memory Message Broker

--- src/main/java/ru/t1academy/config/SupportConfig.java
+++ src/main/java/ru/t1academy/config/SupportConfig.java
@@ -1,20 +1,26 @@
 package ru.t1academy.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import ru.t1academy.controller.SupportController;
 import ru.t1academy.repository.SupportRepository;
 import ru.t1academy.repository.SupportRepositoryImpl;
 import ru.t1academy.service.SupportService;
 import ru.t1academy.service.SupportServiceImpl;
 
+@Configuration
 public class SupportConfig {
+    @Bean
     public SupportRepository supportRepository() {
         return new SupportRepositoryImpl();
     }
 
+    @Bean
     public SupportService supportService(SupportRepository supportRepository) {
         return new SupportServiceImpl(supportRepository);
     }
 
+    @Bean
     public SupportController supportController(SupportService supportService) {
         return new SupportController(supportService);
     }
